2bebfec5cf3a14ab1d51dce877c06aad25a61689
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-06-27 04:59:53 -0300

    Added sign-in and sign-up clerk components

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,5 @@
+import { SignIn } from "@clerk/nextjs";
+
 export default function SignInPage() {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+  return <SignIn path="/sign-in" />;
 }
