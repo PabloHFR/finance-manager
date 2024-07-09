0b1d2badb727cff1c4abd453411a02cdcc31dfff
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-09 11:04:12 -0300

    Fixed SheetProvider hydration error and added zustand package

--- providers/SheetProvider.tsx
+++ providers/SheetProvider.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { NewAccountSheet } from "@/features/accounts/components/newAccountsSheet";
+import { useMountedState } from "react-use";
 
 export const SheetProvider = () => {
+  const isMounted = useMountedState();
+
+  if (!isMounted) return null;
+
   return (
     <>
       <NewAccountSheet />
