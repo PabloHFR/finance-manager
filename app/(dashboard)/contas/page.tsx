7a6532e99e0cb8ed5d7e40dc740dddc7ac6a4bed
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-12 20:02:04 -0300

    Added Skeleton shadcn-ui component and loading state to accounts data table

--- app/(dashboard)/contas/page.tsx
+++ app/(dashboard)/contas/page.tsx
@@ -4,14 +4,32 @@ import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./_components/Columns";
 import { useGetAccounts } from "@/features/accounts/api/useGetAccounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function AccountsPage() {
   const newAccount = useNewAccount();
   const accountsQuery = useGetAccounts();
   const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
