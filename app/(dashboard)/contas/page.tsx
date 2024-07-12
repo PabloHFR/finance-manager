1f0815076f3f9cf28e981bab7e0649db874aeaa1
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-11 23:29:31 -0300

    Added pagintation and sorting to data table

--- app/(dashboard)/contas/page.tsx
+++ app/(dashboard)/contas/page.tsx
@@ -1,12 +1,36 @@
 "use client";
 
+import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
 import { Plus } from "lucide-react";
+import { columns, Payment } from "./_components/Columns";
 
 export default function AccountsPage() {
   const newAccount = useNewAccount();
+
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "a@example.com",
+    },
+    {
+      id: "728e2d52f",
+      amount: 120,
+      status: "pending",
+      email: "b@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 150,
+      status: "pending",
+      email: "m22@example.com",
+    },
+    // ...
+  ];
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -18,6 +42,9 @@ export default function AccountsPage() {
             Adicionar
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   );
