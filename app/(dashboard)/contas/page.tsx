0686da3372705f6902bc5efb6546ca14ee638eeb
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-11 23:18:28 -0300

    Added card component to accounts page

--- app/(dashboard)/contas/page.tsx
+++ app/(dashboard)/contas/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
+import { Plus } from "lucide-react";
+
 export default function AccountsPage() {
+  const newAccount = useNewAccount();
+
   return (
-    <div>
-      <div></div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Contas</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Adicionar
+          </Button>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
