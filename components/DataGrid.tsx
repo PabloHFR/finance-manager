93b25990c540463f20d7ad35f1342f3c8887dbf0
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-29 09:08:12 -0300

    Added DataCard component and added react-icons package

--- components/DataGrid.tsx
+++ components/DataGrid.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useGetSummary } from "@/features/summary/api/useGetSummary";
 import { formatDateRange } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+import { DataCard } from "./DataCard";
 
 export const DataGrid = () => {
+  const { data } = useGetSummary();
+
   const params = useSearchParams();
   const to = params.get("to") || undefined;
   const from = params.get("from") || undefined;
@@ -12,7 +17,14 @@ export const DataGrid = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-      <div></div>
+      <DataCard
+        title="Restante"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
     </div>
   );
 };
