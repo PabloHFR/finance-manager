e0b767fd8099e54e6cb7e3f737b1837d43b60b06
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-29 12:15:21 -0300

    Added Chart component and added recharts package

--- components/DataCharts.tsx
+++ components/DataCharts.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useGetSummary } from "@/features/summary/api/useGetSummary";
+import { Chart } from "./Chart";
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary();
@@ -15,7 +16,9 @@ export const DataCharts = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
-      <div className="col-span-1 lg:col-span-3 xl:col-span-4"></div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <Chart data={data?.days} />
+      </div>
     </div>
   );
 };
