e2a0a9fe777013938f9267fbe5339991d882a7b7
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-07-30 09:20:35 -0300

    Added DateFilter component

--- components/AccountFilter.tsx
+++ components/AccountFilter.tsx
@@ -10,6 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { useGetSummary } from "@/features/summary/api/useGetSummary";
 
 export const AccountFilter = () => {
   const pathname = usePathname();
@@ -21,6 +22,7 @@ export const AccountFilter = () => {
   const to = params.get("to") || "";
 
   const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
+  const { isLoading: isLoadingSummary } = useGetSummary();
 
   const onChange = (newValue: string) => {
     const query = {
@@ -48,7 +50,7 @@ export const AccountFilter = () => {
     <Select
       value={accountId}
       onValueChange={onChange}
-      disabled={isLoadingAccounts}
+      disabled={isLoadingAccounts || isLoadingSummary}
     >
       <SelectTrigger className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition">
         <SelectValue placeholder="Selecionar conta" />
