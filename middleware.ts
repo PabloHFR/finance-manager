e45cdbaacbd3b05f6cbe6a3c1faea5bd2bc1f622
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2024-08-05 07:10:36 -0300

    Added routes to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/accounts(.*)",
+  "/categories(.*)",
+  "/transactions(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
